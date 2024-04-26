b6408d2467be09b2ccec893efcee15992915f0a0
sakariB1 <zachary.burda@gmail.com>
2024-04-26 16:18:53 +0300

    js to tsx and text changes

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Here is our app for SmartBi challenge!
-        The stuff to do:
-        1. integrate CV to the app/create a backend server which will process video (open app, chose video from gallery, analise it)
-        2. make front-end using tsx format
-        3. Create a backend server  
-      </Text>
+      <Text>Here is our app for SmartBi challenge!</Text>
+      <Text>The stuff to do:</Text>
+      <Text>1. Integrate CV to the app/create a backend server {"\n"} which will process video  {"\n"}(open app, chose video from gallery, analyse it)</Text>
+      <Text>2. Make front-end using tsx format</Text>
+      <Text>3. Create a backend server</Text>
       <StatusBar style="auto" />
     </View>
   );
