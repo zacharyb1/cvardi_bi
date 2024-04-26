1a972f43a284054efc756a0aa493685c42a6a3da
junezhuo <yzzhuo2@gmail.com>
2024-04-26 21:40:02 +0300

    feat: onboarding UI

--- App.tsx
+++ App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Text, View , Button} from 'react-native';
+import Avatar from './components/Avatar';
 
+interface UserInfo {
+  username: string;
+  photo: string;
+}
 export default function App() {
+  const [userInfo, onChangeUserInfo] = React.useState<UserInfo>({
+    username: '',
+    photo: '',
+  });
+
+  const [currentStep, setCurrentStep] = React.useState(1);
+  const handleNext = () => {
+    if (currentStep === 0 && userInfo.username === '') {
+      alert('Please pick a username');
+      return;
+    }
+    setCurrentStep(currentStep + 1);
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Here is our app for SmartBi challenge!</Text>
-      <Text>The stuff to do:</Text>
-      <Text>1. Integrate CV to the app/create a backend server {"\n"} which will process video  {"\n"}(open app, chose video from gallery, analyse it)</Text>
-      <Text>2. Make front-end using tsx format</Text>
-      <Text>3. Create a backend server</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View className="bg-white py-24 px-8">
+      <Text className='text-5xl	font-bold'>Welcome</Text>
+      <View className='mt-4 flex flex-col'>
+        {currentStep === 0 && <TextInput
+          onChangeText={(text) => onChangeUserInfo({ ...userInfo, username: text })}
+          value={userInfo.username}
+          placeholder='Please pick a username'
+          className='border-2 border-gray-300 h-14 leading-normal rounded-lg px-2 w-full mt-4 text-md flex items-center'
+        />
+        }
+        {currentStep === 1 && <Avatar />}
+        <View className='bg-black p-2 rounded-lg mt-4 text-white'>
+          <Button
+            title="Next"
+            onPress={handleNext}
+            color="#fff"
+            />
+        </View>
+      </View>
+    <StatusBar style="auto" />
+  </View>
   );
 }
