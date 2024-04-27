ffba9e43224f7a0a8ef35502b9d6e954933738e0
Alireza Fatemi <seyedalirezafatemijahromi@gmail.com>
2024-04-27 15:04:42 +0300

    final stream

--- backend/stream.py
+++ backend/stream.py
@@ -1,34 +1,65 @@
 import asyncio
 import base64
+import queue
+import threading
+import time
 
 import cv2
 import numpy as np
 import websockets
 
 
-async def send_image(frame: np.ndarray):
-    async with websockets.connect("ws://65.108.33.114:8765") as websocket:
-        try:
-            # Convert the frame to bytes
-            _, buffer = cv2.imencode(".jpg", frame)
-            image_bytes = buffer.tobytes()
+# bufferless VideoCapture
+class VideoCapture:
+    def __init__(self, name):
+        self.cap = cv2.VideoCapture(name)
+        self.q = queue.Queue()
+        t = threading.Thread(target=self._reader)
+        t.daemon = True
+        t.start()
 
-            # Encode the image data as base64
-            image_data = base64.b64encode(image_bytes).decode()
+    # read frames as soon as they are available, keeping only most recent one
+    def _reader(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            if not self.q.empty():
+                try:
+                    self.q.get_nowait()  # discard previous (unprocessed) frame
+                except queue.Empty:
+                    pass
+            self.q.put(frame)
 
-            # Send the image data to the server
-            await websocket.send(image_data)
-            print("Image sent successfully!")
+    def read(self):
+        return self.q.get()
 
-        except Exception as e:
-            print(f"Error: {e}")
+
+async def send_image(nocap: VideoCapture):
+    async with websockets.connect("ws://65.108.33.114:8765", max_size=2**20 * 5) as websocket:
+        while True:
+            try:
+                # Convert the frame to bytes
+                frame = nocap.read()
+                _, buffer = cv2.imencode(".jpg", frame)
+                image_bytes = buffer.tobytes()
+
+                # Encode the image data as base64
+                image_data = base64.b64encode(image_bytes).decode()
+
+                # Send the image data to the server
+                await websocket.send(image_data)
+                print("Image sent successfully! Sleeping for 2 seconds...")
+                await asyncio.sleep(2)
+                print("Waking up...")
+            except Exception as e:
+                print(f"Error: {e}")
 
 
 def main():
     rtsp_url = "rtsp://aaltoai:aaltoai@172.20.10.14:554/stream1"
-    cap = cv2.VideoCapture(rtsp_url)
-    ret, frame = cap.read()
-    asyncio.get_event_loop().run_until_complete(send_image(frame))
+    nocap = VideoCapture(rtsp_url)
+    asyncio.get_event_loop().run_until_complete(send_image(nocap))
 
 
 if __name__ == "__main__":
